52580707a51378a58d6105b4856ed41ba9fa9ac5
Constantin Clauzel <stantin04@gmail.com>
2015-09-27 10:25:26 +0200

    added post/put, made tests

--- parse.go
+++ parse.go
@@ -34,27 +34,55 @@ func InitAPI(appId, apiKey string) {
  */
 
 type Object struct {
+	changedKeys []string
 	fields map[string]interface{}
 }
 
 // Constructor
-func NewObject(fields map[string]interface{}) *Object {
-	if fields == nil {
-		fields = map[string]interface{}{}
+func NewObject() *Object {
+	changedKeys := []string{}
+	fields := map[string]interface{}{}
+
+	return &Object{changedKeys, fields}
+}
+
+func (o *Object) setKeyChanged(key string) {
+	if inArray(o.changedKeys, key) == false {
+		o.changedKeys = append(o.changedKeys, key)
 	}
-	o := &Object{map[string]interface{}{}}
-	o.SetFromMap(fields)
-	return o
+}
+
+func (o *Object) didKeyChange(key string) bool {
+	if obj, ok := o.GetObject(key); ok == true {
+		return obj.hasChanges()
+	}
+	return inArray(o.changedKeys, key)
+}
+
+func (o *Object) hasChanges() bool {
+	return len(o.changedKeys) != 0
+}
+
+func (o *Object) clearChangedKeys() {
+	for _, v := range o.fields {
+		if obj, ok := v.(*Object); ok == true {
+			obj.clearChangedKeys()
+		}
+	}
+	o.changedKeys = []string{}
 }
 
 func (o *Object) SetFromMap(fields map[string]interface{}) {
 	for key, value := range fields {
 		if v, ok := value.(map[string]interface{}); ok == true {
-			o.Set(key, NewObject(v))
+			nested := NewObject()
+			nested.SetFromMap(v)
+			o.Set(key, nested)
 		} else {
 			o.Set(key, value)
 		}
 	}
+	o.clearChangedKeys()
 }
 
 // MarshalJSON, pass `fields` as own JSON representation
@@ -73,6 +101,7 @@ func (o *Object) Push(key string, value interface{}) {
 }
 
 func (o *Object) Set(key string, value interface{}) {
+	o.setKeyChanged(key)
 	o.fields[key] = value
 }
 
@@ -81,29 +110,46 @@ func (o *Object) Get(key string) (value interface{}, ok bool) {
 	return
 }
 
-// Typed methods
+func (o *Object) Remove(key string) {
+	delete(o.fields, key)
+	o.setKeyChanged(key)
+}
 
+// Typed methods
+// TODO check type inference
 func (o *Object) GetString(key string) (value string, ok bool) {
 	v, ok := o.Get(key)
-	value = v.(string)
+	if ok == false {
+		return
+	}
+	value, ok = v.(string)
 	return
 }
 
 func (o *Object) GetInt(key string) (value int, ok bool) {
 	v, ok := o.Get(key)
-	value = v.(int)
+	if ok == false {
+		return
+	}
+	value, ok = v.(int)
 	return
 }
 
 func (o *Object) GetFloat(key string) (value float64, ok bool) {
 	v, ok := o.Get(key)
-	value = v.(float64)
+	if ok == false {
+		return
+	}
+	value, ok = v.(float64)
 	return
 }
 
 func (o *Object) GetObject(key string) (value *Object, ok bool) {
 	v, ok := o.Get(key)
-	value = v.(*Object)
+	if ok == false {
+		return
+	}
+	value, ok = v.(*Object)
 	return
 }
 
@@ -111,7 +157,7 @@ func (o *Object) GetObject(key string) (value *Object, ok bool) {
 func (o *Object) AssertObject(key string) (*Object, error) {
 	v, ok := o.Get(key)
 	if ok == false {
-		v = NewObject(make(map[string]interface{}))
+		v = NewObject()
 		o.Set(key, v)
 		return v.(*Object), nil
 	}
@@ -119,6 +165,21 @@ func (o *Object) AssertObject(key string) (*Object, error) {
 		return nil, fmt.Errorf("%s is already set with non object type", key)
 	}
 	return v.(*Object), nil
+}
+
+// toMap
+func (o *Object) toMap(filter func(*Object, string) bool) map[string]interface{} {
+	fields := map[string]interface{}{}
+	for k, v := range o.fields {
+		if filter(o, k) {
+			if obj, ok := v.(*Object); ok == true {
+				fields[k] = obj.toMap(filter)
+			} else {
+				fields[k] = v
+			}
+		}
+	}
+	return fields
 }
 
 /**
@@ -132,7 +193,7 @@ type ClassObject struct {
 
 // Constructors
 func NewClassObject(class string) *ClassObject {
-	co := &ClassObject{Object: *NewObject(nil), class: class}
+	co := &ClassObject{Object: *NewObject(), class: class}
 	return co
 }
 
@@ -145,48 +206,152 @@ func GetClassObject(class string, objectId string) (*ClassObject, error) {
 	return co, nil
 }
 
+// Create a copy of this object, but removes Parse read only fields
+func (co *ClassObject) postMap() map[string]interface{} {
+	excluded := []string{"parsemap_identifier", "updatedAt", "createdAt", "objectId"}
+	fields := co.toMap(func(o *Object, key string) bool {
+		return !inArray(excluded, key)
+	})
+	return fields
+}
+
+func (co *ClassObject) putMap() map[string]interface{} {
+	excluded := []string{"parsemap_identifier", "updatedAt", "createdAt", "objectId"}
+	fields := co.toMap(func(o *Object, key string) bool {
+		return !inArray(excluded, key) && ((o == &co.Object && o.didKeyChange(key)) || (o != &co.Object && o.hasChanges()))
+	})
+	return fields
+}
+
 // server methods
 func (co *ClassObject) Save() error {
 	if _, ok := co.Get("objectId"); ok == true {
-		co.put()
+		return co.put()
 	} else {
-		co.post()
+		return co.post()
 	}
-	return nil
 }
 
 func (co *ClassObject) Delete() error {
-	return nil
+	return co.del()
 }
 
 // private methods
 func (co *ClassObject) get() error {
-	objectId, ok := co.fields["objectId"]
+	objectId, ok := co.Get("objectId")
 	if ok == false {
 		return fmt.Errorf("Object should have a `objectId` field to be gettable")
 	}
 	url := fmt.Sprintf("%s/classes/%s/%s", PARSE_URL, co.class, objectId)
-	data := map[string]interface{}{}
-	response, err := session.Get(url, nil, &data, nil)
+
+	errorData := map[string]interface{}{}
+	
+	responseData := map[string]interface{}{}
+	response, err := session.Get(url, nil, &responseData, errorData)
 	if err != nil {
 		return err
 	}
-	if response.Status() == 200 {
-		co.SetFromMap(data)
+	if statusOk(response.Status()) {
+		co.SetFromMap(responseData)
 	} else {
-		return fmt.Errorf("response status for Get requests should be 200, got %d", response.Status())
+		errorJson, err := json.Marshal(errorData)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("response status for Get requests should be 200, got %d\nerror: %s", response.Status(), errorJson)
 	}
 	return nil
 }
 
 func (co *ClassObject) post() error {
+	url := fmt.Sprintf("%s/classes/%s", PARSE_URL, co.class)
+	
+	errorData := map[string]interface{}{}
+	
+	requestObj := co.postMap()
+	responseData := map[string]interface{}{}
+	response, err := session.Post(url, requestObj, &responseData, &errorData)
+	if err != nil {
+		return err
+	}
+	if !statusOk(response.Status()) {
+		errorJson, err := json.Marshal(errorData)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("response status for Get requests should be 200, got %d\nerror: %s", response.Status(), errorJson)
+	} else {
+		co.Set("objectId", responseData["objectId"])
+	}
+	co.clearChangedKeys()
 	return nil
 }
 
 func (co *ClassObject) put() error {
+	objectId, ok := co.Get("objectId")
+	if ok == false {
+		return fmt.Errorf("Object should have a `objectId` field to be puttable")
+	}
+	url := fmt.Sprintf("%s/classes/%s/%s", PARSE_URL, co.class, objectId)
+
+	errorData := map[string]interface{}{}
+	
+	requestObj := co.putMap()
+	responseData := map[string]interface{}{}
+	response, err := session.Put(url, requestObj, &responseData, &errorData)
+	if err != nil {
+		return err
+	}
+	if statusOk(response.Status()) {
+		co.SetFromMap(responseData)
+	} else {
+		errorJson, err := json.Marshal(errorData)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("response status for Get requests should be 200, got %d\nerror: %s", response.Status(), errorJson)
+	}
+	co.clearChangedKeys()
 	return nil
 }
 
 func (co *ClassObject) del() error {
+	objectId, ok := co.Get("objectId")
+	if ok == false {
+		return fmt.Errorf("Object should have a `objectId` field to be deletable")
+	}
+	url := fmt.Sprintf("%s/classes/%s/%s", PARSE_URL, co.class, objectId)
+
+	errorData := map[string]interface{}{}
+	
+	responseData := map[string]interface{}{}
+	response, err := session.Delete(url, &responseData, &errorData)
+	if err != nil {
+		return err
+	}
+	if !statusOk(response.Status()) {
+		errorJson, err := json.Marshal(errorData)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("response status for Get requests should be 200, got %d\nerror: %s", response.Status(), errorJson)
+	}
 	return nil
+}
+
+/**
+ *	Misc
+ */
+
+func statusOk(status int) bool {
+	return status >= 200 && status < 300
+}
+
+func inArray(keys []string, key string) bool {
+	for _, k := range keys {
+		if k == key {
+			return true
+		}
+	}
+	return false
 }
